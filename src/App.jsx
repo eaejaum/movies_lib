9d54b3d221993e12019a2a5c4c5552c18d0459fe
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-11 18:58:32 -0300

    navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { Link, Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h1>React App</h1>
+      <Outlet />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Movie from "./pages/Movie.jsx";
+import Search from "./pages/Search.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Router path="/" element={<Home />}/>
-          <Router path="movie/:id" element={<Movie />}/>
-          <Router path="/" element={<Search />}/>
+          <Route path="/" element={<Home />}/>
+          <Route path="/movie/:id" element={<Movie />}/>
+          <Route path="/search" element={<Search />}/>
         </Route>
       </Routes>
     </BrowserRouter>
