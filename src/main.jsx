a139d223103c58f643c56027b9c4c604be722555
João Pedro Santos <jpedroalmeida179@gmail.com>
2025-03-11 18:50:57 -0300

    cleaning project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Router path="/" element={<Home />}/>
+          <Router path="movie/:id" element={<Movie />}/>
+          <Router path="/" element={<Search />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
